02e9fca0ddf8b1849c4fc3835854837f42968c8d
unknown <sohammukherjee2000@gmail.com>
2022-11-13 02:18:43 -0600

    Updated Home, added emoji to Help Page

--- frontend/Home.py
+++ frontend/Home.py
@@ -2,10 +2,10 @@ import streamlit as st
 
 st.set_page_config(
     page_title="Hello World",
-    page_icon="👋",
+    page_icon="🏠",
 )
 
-st.write("# Welcome to DataStacks! 👋")
+st.write("# Welcome to DataStacks! 🏠")
 
 st.markdown(
     """
